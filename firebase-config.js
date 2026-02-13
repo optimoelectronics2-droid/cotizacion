ea01b5d9a1e3e7e60a4451c5ed89611c3771a688
optimoelectronics2-droid <optimoelectronics2@gmail.com>
2026-02-13 16:20:23 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,16 @@
 // firebase-config.js
-// Incluir Firebase SDK antes de esta configuración
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDlOJqBgd-8KqUT-Y94lsma5W8T79PsPjM",
-  authDomain: "trifusion-cotizador.firebaseapp.com",
-  projectId: "trifusion-cotizador",
-  storageBucket: "trifusion-cotizador.firebasestorage.app",
-  messagingSenderId: "407374211882",
-  appId: "1:407374211882:web:b692792ccc39c8d48f4c53"
+    apiKey: "AIzaSyDlOJqBgd-8KqUT-Y94lsma5W8T79PsPjM",
+    authDomain: "trifusion-cotizador.firebaseapp.com",
+    projectId: "trifusion-cotizador",
+    storageBucket: "trifusion-cotizador.firebasestorage.app",
+    messagingSenderId: "407374211882",
+    appId: "1:407374211882:web:b692792ccc39c8d48f4c53"
 };
 
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Accesos globales
+// Accesos globales - SIN storage (no se usa en este sistema)
 const db = firebase.firestore();
 const auth = firebase.auth();
-const storage = firebase.storage();
